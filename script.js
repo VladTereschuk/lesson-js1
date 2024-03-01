7ac38a4a916b2afb6fe320c8d69f5d26bef626a0
Volodia <vladtereschuk2000@yahoo.com>
2024-03-01 19:04:05 +0200

    added func firstCapitalLetter

--- script.js
+++ script.js
@@ -19,3 +19,29 @@ function makeCapitalText(inputText){
     return text;
 }
 console.log(makeCapitalText("hello world"));
+
+function firstCapitalLetter(inputText)
+{
+    let text = "";
+    let smallLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    let bigLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    for(let i=0;i<inputText.length;i++)
+    {
+        // 1. if prevoius symbol is " " or current position is 0
+        // 2. getting indexof of current symbol
+        // 3. adding bigletter
+        // 4. if prevoius symbol is not " " adding symbol without changing
+            
+        if(inputText[i-1] == " " || i == 0)
+        {
+            let index = smallLetters.indexOf(inputText[i]);
+            text = text + bigLetters[index];
+        }
+        else{
+            text = text + inputText[i];
+        }
+        
+    }
+    return text;
+}
+console.log(firstCapitalLetter("hello world"));
